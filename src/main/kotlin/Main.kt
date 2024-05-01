d900540f6cdde538b0ce99bd4e4ffa305db253aa
aaracan246 <acalberto@iesrafaelalberti.es>
2024-05-02 00:21:47 +0200

    Reformateado correcto + título + fotito

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,30 @@
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import java.io.File
+import java.io.InputStream
+
 
 @Composable
 fun AppListaAlumn(
@@ -20,13 +34,14 @@ fun AppListaAlumn(
     onSaveChanges: () -> Unit
 ) {
     var nombreUsuario by remember { mutableStateOf("") }
+    val focusRequester = remember { FocusRequester() }
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize()
     ) {
         Row(
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
             verticalAlignment = Alignment.CenterVertically
         ) {
 
@@ -35,6 +50,16 @@ fun AppListaAlumn(
                 onValueChange = { nombreUsuario = it },
                 label = { Text(text = "Student's name: ") },
                 modifier = Modifier.padding(16.dp)
+                    .focusRequester(focusRequester).onKeyEvent { event ->
+                        if (event.key == Key.Enter && nombreUsuario.isNotBlank()){
+                            onAddAlumno(nombreUsuario)
+                            nombreUsuario = ""
+                            focusRequester.requestFocus()
+                            true
+
+                        }
+                        else{ false }
+                    }
             )
 
             Button(
@@ -43,31 +68,39 @@ fun AppListaAlumn(
             ) {
                 Text("Add new student")
             }
-        }
 
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            LazyColumn(
-                modifier = Modifier.weight(1f)
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
             ) {
-                items(alumnos) { alumno ->
-                    Text(alumno, modifier = Modifier.padding(8.dp))
+                Box(
+                    modifier = Modifier
+                        .background(color = Color.Cyan)
+                        .border(width = 1.dp, color = Color.Black)
+                        //.verticalScroll(enabled = true, state = ScrollState(0))
+
+                ){
+                    LazyColumn(
+                    ) {
+                        items(alumnos) { alumno ->
+                            Text(alumno, modifier = Modifier.padding(8.dp))
+                        }
+                    }
                 }
-            }
 
-            Button(
-                onClick = onClearAll,
-                modifier = Modifier.padding(top = 16.dp, bottom = 32.dp)
-            ) {
-                Text("Clear all")
-            }
-            Button(
-                onClick = onSaveChanges,
-                modifier = Modifier.padding(bottom = 16.dp)
-            ) {
-                Text("Save changes")
+                Button(
+                    onClick = onClearAll,
+                    modifier = Modifier.padding(top = 16.dp, bottom = 32.dp)
+                ) {
+                    Text("Clear all")
+                }
+
+                Button(
+                    onClick = onSaveChanges,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                ) {
+                    Text("Save changes")
+                }
             }
         }
 
@@ -111,7 +144,16 @@ fun main() = application {
         alumnos.add(alumno)
     }
 
-    Window(onCloseRequest = ::exitApplication) {
+    val imageFile = File("src/main/resources/Sample1.png")
+    val inputStream: InputStream = imageFile.inputStream()     // <-- Buscar cómo saltar este paso / no me ha dejado cargar la imagen del tirón
+
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "AppListaAlumnos",
+        icon = BitmapPainter(image = loadImageBitmap(inputStream))
+    ) {
+
+
         var savedAlumnos by remember { mutableStateOf(alumnos) }
 
         AppListaAlumn(
@@ -131,3 +173,66 @@ fun main() = application {
         )
     }
 }
+
+
+
+/*@Composable
+	fun Toast(message: String, onDismiss: () -> Unit) {
+		Dialog(
+			icon = painterResource("info_icon.png"),
+			title = "Atención",
+			resizable = false,
+			onCloseRequest = onDismiss
+		) {
+			Box(
+				contentAlignment = Alignment.Center,
+				modifier = Modifier.fillMaxSize().padding(16.dp)
+			) {
+				Text(message)
+			}
+		}
+		// Cierra el Toast después de 2 segundos
+		LaunchedEffect(Unit) {
+			delay(2000)
+			onDismiss()
+		}
+	}
+* TooltipArea:
+
+Se utiliza para proporcionar información contextual en forma de una pequeña ventana emergente (tooltip) cuando el usuario interactúa con el componente de la interfaz, por ejemplo, al pasar el cursor sobre un elemento en una aplicación de escritorio.
+
+    TooltipArea(
+        tooltip = {
+            // Composable con la info que se quiera mostrar...
+        }
+    ) {
+        // Composable sobre el que se mostrará la info contextual
+    }
+* onKeyEvent:
+
+Es un manejador de eventos que te permite reaccionar a eventos de teclado que ocurren en un composable específico. Es especialmente útil en desarrollos de escritorio o en aplicaciones web que necesitan interactividad a través del teclado.
+
+En resumen, para manejar los eventos de pulsaciones de teclas en un composable... por ejemplo en la LazyColumn dónde se mostrarán los estudiantes:
+
+	modifier = Modifier
+                .onKeyEvent { event ->
+                    if (event.type == KeyEventType.KeyUp) {
+                        when (event.key) {
+                            Key.DirectionUp -> {
+                                if (selectedIndex > 0) {
+                                    onStudentSelected(selectedIndex - 1)
+                                    true
+                                } else false
+                            }
+                            Key.DirectionDown -> {
+                                if (selectedIndex < studentsState.size - 1) {
+                                    onStudentSelected(selectedIndex + 1)
+                                    true
+                                } else false
+                            }
+                            else -> false
+                        }
+                    } else {
+                        false//Solo manejar cuando la tecla se haya levantado de la presión
+                    }
+                }*/
